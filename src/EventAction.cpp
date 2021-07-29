933b7b8f5e35fef34b7904b2d0ad60714f4cfc61
Johnny Ho <johnnyho@fas.harvard.edu>
2021-07-29 15:32:46 -0400

    add event ID offset

--- src/EventAction.cpp
+++ src/EventAction.cpp
@@ -14,14 +14,20 @@
 
 // GEANT4 includes
 #include "G4Event.hh"
+#include "G4GenericMessenger.hh"
 
 
-EventAction::EventAction(): G4UserEventAction()
-{}
+EventAction::EventAction():
+  G4UserEventAction(), event_id_offset_(0)
+{
+    msg_ = new G4GenericMessenger(this, "/event/", "user-defined event configuration");
+    msg_->DeclareProperty("offset", event_id_offset_, "Event ID offset.");
+}
 
 
 EventAction::~EventAction()
-{}
+{
+}
 
 
 void EventAction::BeginOfEventAction(const G4Event*)
@@ -33,11 +39,28 @@ void EventAction::EndOfEventAction(const G4Event* event)
     // get MC truth manager
     MCTruthManager * mc_truth_manager = MCTruthManager::Instance();
 
+    // get map of particles from MC truth manager
+    auto const MCParticleMap = mc_truth_manager->GetMCParticleMap();
+
+    double energy_deposited = 0.;
+
+    // add particle to analysis manager
+    for (auto const& p : MCParticleMap)
+    {
+        auto const& particle = p.second;
+        energy_deposited += particle->EnergyDeposited();
+        // std::cout << "Energy deposited by particle PDG (" << particle->PDGCode() << "): " << particle->EnergyDeposited() << std::endl;
+    }
+
+    // std::cout << "Total energy deposited: " << energy_deposited << std::endl;
+
     // get analysis manager
     AnalysisManager * analysis_manager = AnalysisManager::Instance();
 
     // set event number
-    analysis_manager->SetEvent(event->GetEventID());
+    // event->SetEventID(event->GetEventID() + event_id_offset_);
+    // analysis_manager->SetEvent(event->GetEventID());
+    analysis_manager->SetEvent(event->GetEventID() + event_id_offset_);
 
     // add initial generator particles to analysis manager
     for (auto const& particle : mc_truth_manager->GetInitialGeneratorParticles())
@@ -52,12 +75,13 @@ void EventAction::EndOfEventAction(const G4Event* event)
     }
 
     // get map of particles from MC truth manager
-    auto const MCParticleMap = mc_truth_manager->GetMCParticleMap();
+    // auto const MCParticleMap = mc_truth_manager->GetMCParticleMap();
 
     // add particle to analysis manager
     for (auto const& p : MCParticleMap)
     {
         auto const& particle = p.second;
+
         analysis_manager->AddMCParticle(particle);
     }
 
--- src/EventAction.h
+++ src/EventAction.h
@@ -12,13 +12,21 @@
 #include <G4UserEventAction.hh>
 
 
+class G4GenericMessenger;
+
 class EventAction: public G4UserEventAction
 {
-public:
-  EventAction();
-  virtual ~EventAction();
-  virtual void BeginOfEventAction(const G4Event*);
-  virtual void EndOfEventAction(const G4Event*);
+    public:
+
+        EventAction();
+        virtual ~EventAction();
+        virtual void BeginOfEventAction(const G4Event*);
+        virtual void EndOfEventAction(const G4Event*);
+
+    private:
+
+        G4GenericMessenger* msg_; // Messenger for configuration parameters
+        int event_id_offset_;
 };
 
 #endif
