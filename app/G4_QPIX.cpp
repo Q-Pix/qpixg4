70bfce74886f13f9e51e1a8df16f00948bcb3c2c
Dave Elofson <dave.elofson@tamu.edu>
2022-10-14 12:55:11 -0500

    adding getopt to G4_QPIX.cpp

--- app/G4_QPIX.cpp
+++ app/G4_QPIX.cpp
@@ -12,18 +12,31 @@
 #include "EventAction.h"
 #include "TrackingAction.h"
 #include "SteppingAction.h"
-
+#include "../src/ROOTManager.h"
 #include <G4RunManager.hh>
 #include <G4UImanager.hh>
 #include <G4VisExecutive.hh>
 #include <G4UIExecutive.hh>
 #include <FTFP_BERT_HP.hh>
 #include <G4EmStandardPhysics_option4.hh>
+#include <G4OpticalPhysics.hh>
 
 
 #include "Randomize.hh"
 #include "time.h"
+#include <getopt.h>
 
+
+void PrintHelp(){
+    G4cerr << "\nUsage: ./build/app/G4_QPIX [-i <root_file>] [-g <generator>] [-f <root_file_format>] -m <macro>\n"  << G4endl;
+    G4cerr << "Available options: " <<G4endl;
+    G4cerr << "-i --input --> input .root file  " <<G4endl;
+    G4cerr << "-g --generator --> specify which generator created the file   " <<G4endl;
+    G4cerr << "-f --format  --> specify format of the input file " <<G4endl;
+    G4cerr << "-m --macro  --> specify the macro file to use (REQUIRED) " <<G4endl;
+    G4cerr << "-h --help --> Help" <<G4endl;
+    exit(EXIT_FAILURE);
+}
 
 
 int main(int argc, char** argv)
@@ -34,6 +47,7 @@ int main(int argc, char** argv)
   //set random seed with system time
   G4long seed = time(NULL);
   CLHEP::HepRandom::setTheSeed(seed);
+
 
 
   // Detect interactive mode (if no arguments) and define UI session
@@ -69,9 +83,83 @@ int main(int argc, char** argv)
   //
   if (!ui) {
     // batch mode
+    const char* const short_opts = "i::g::f::m:h";
+    const option long_options[] = {
+      {"input",       optional_argument,            nullptr,   'i'},
+      {"generator",   optional_argument,            nullptr,   'g'},
+      {"format",      optional_argument,            nullptr,   'f'},
+      {"macro",       required_argument,            nullptr,   'm'},
+      {"help",        no_argument,                  nullptr,   'h'},
+      {0, 0, 0, 0};
+    };
+
+    string macro_name = "";
+
+    while (1) {
+
+      const auto opt = getopt_long(argc, argv, short_opts, long_opts, nullptr);
+
+      if (opt == -1)
+        break;
+
+      switch (opt) {
+
+        case 'i':
+          uimgr->SetAlias("ROOT_FILE "+optarg);
+          break;
+
+        case 'g':
+          uimgr->SetAlias("GENERATOR "+optarg);
+          break;
+
+        case 'f':
+          uimgr->SetAlias("INPUT_FORMAT "+optarg);
+          break;
+
+        case 'm':
+          macro_name = optarg;
+          break;
+ 
+        case 'h':
+          PrintHelp();
+          break; 
+
+        case '?':
+          PrintHelp();
+          break;
+
+        default:
+          abort();
+      }
+    }
+
+    // Still allow the submission of jobs using the old format.
+    // If there are no options specified, but still arguments, assume the old method
+    if (optind < argc && argc - optind == 1) {
+      printf("Macro file entered old way. Using macro file: ");
+      while (optind < argc) {
+        printf("%s ", argv[optind++]);
+        macro_name = argv[optind++];
+      }
+      printf("\n");
+    }
+  
+    // Check that there are no extra non-option arguments. If so, print help message and abort.
+    if (optind < argc && argc - optind > 1) {
+      printf("Too many non-option arguments entered! Non-option arguments entered");
+      while (optind < argc) {
+        printf("%s ", argv[optind++]);
+      }
+      printf("\n");
+      printf("You must either submit batch job using options (see help) or in the old way: ./Build/G4_QPIX [insert_macro_file]");
+      PrintHelp();
+      abort()
+    }
+
+    // Execute Macro file using any aliases set above
     G4String command = "/control/execute ";
-    G4String fileName = argv[1];
-    uimgr->ApplyCommand(command+fileName);
+    uimgr->ApplyCommand(command+macro_name);
+    
   }
   else {
     // interactive mode
