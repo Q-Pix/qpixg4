a7ad7edd5ceb0642fd39b917cfc52a8921b0c3ee
AustinMcDonald <austin.mcdonald96@mavs.uta.edu>
2023-02-24 13:35:46 -0600

    dataframe sort macro, combine witlongdecay script

--- LongDecay.sh
+++ LongDecay.sh
@@ -2,54 +2,83 @@
 
 outputdir="/home/argon/Projects/Kevin/qpixg4/output/"
 outputMacroDir="/home/argon/Projects/Kevin/qpixg4/macros/long_macros/"
+prog="/home/argon/Projects/Kevin/qpixg4/build/app/G4_QPIX"
 NPROC=10
 
+# clean macrodir
+rm "$outputMacroDir""/*"
+
 function makeMacroFile {
-	touch "$outputMacroDir""$1"".mac"
 	dest="$outputMacroDir""$1"".mac"
 
 	echo "making file: $dest"
+  if test -f "$dest"; then
+    rm "$dest"
+  fi
+  touch "$dest"
 
 	# set verbosity
 	echo "/control/verbose 1" >> $dest
 	echo "/run/verbose 1" >> $dest
 	echo "/tracking/verbose 0" >> $dest
 
-	# # configure supernova
+	# configure supernova
 	echo "/Inputs/Particle_Type SUPERNOVA" >> $dest
 
-	# # output path
+	# output path
 	echo "/Inputs/root_output $outputdir$1_$2.root" >> $dest
 
-	# # initialize run
+	# initialize run
 	echo "/run/initialize" >> $dest
 	echo "/random/setSeeds $2 707010" >> $dest
 
-	# # event ID offset
+	# event ID offset
 	echo "/event/offset 0" >> $dest
 
-	# # Supernova configs
-	echo "/Supernova/Event_Window 100000 s" >> $dest
-	echo "/Supernova/Event_Cutoff 100000 s" >> $dest
+	# Supernova configs
+	echo "/Supernova/Event_Window 10 s" >> $dest
+	echo "/Supernova/Event_Cutoff 10 s" >> $dest
 
-	echo "/Supernova/N_Ar39_Decays 1" >> $dest
+  # single macro
+  # echo "/Supernova/N_Ar39_Decays 707000" >> $dest
+  # echo "/Supernova/N_Ar42_Decays 64" >> $dest
+  # echo "/Supernova/N_Bi214_Decays 7000" >> $dest
+  # echo "/Supernova/N_Co60_Decays 41" >> $dest
+  # echo "/Supernova/N_K40_Decays 12642" >> $dest
+  # echo "/Supernova/N_K42_Decays 64" >> $dest
+  # echo "/Supernova/N_Kr85_Decays 80500" >> $dest
+  # echo "/Supernova/N_Pb214_Decays 7000" >> $dest
+  # echo "/Supernova/N_Po210_Decays 5" >> $dest
+  # echo "/Supernova/N_Rn222_Decays 27740" >> $dest
+	# echo "/run/beamOn 1" >> $dest
 
-	# # run
-	echo "/run/beamOn 707000" >> $dest
+  # multiple macros
+  echo "/Supernova/$3 1" >> $dest
+	echo "/run/beamOn $4" >> $dest
 }
 
-prog="/home/argon/Projects/Kevin/qpixg4/build/app/G4_QPIX"
-function runG4 {
+file="_Long_Decay"
+# single macro
+# makeMacroFile "Single"$file 2
 
-}
+# multiple macros
+makeMacroFile "Ar39"$file 3 "N_Ar39_Decays" 707000
+makeMacroFile "Ar42"$file 3 "N_Ar42_Decays" 64
+makeMacroFile "Bi214"$file 3 "N_Bi214_Decays" 7000
+makeMacroFile "Co60"$file 3 "N_Co60_Decays" 41
+makeMacroFile "K40"$file 3 "N_K40_Decays" 12642
+makeMacroFile "K42"$file 3 "N_K42_Decays" 64
+makeMacroFile "Kr85"$file 3 "N_Kr85_Decays" 80500
+makeMacroFile "Pb214"$file 3 "N_Pb214_Decays" 7000
+makeMacroFile "Po210"$file 3 "N_Po210_Decays" 5
+makeMacroFile "Rn222"$file 3 "N_Rn222_Decays" 27740
 
-
-for i in {1..10}
+mac_array=( $( ls "$outputMacroDir""/" ) )
+## now loop through the above array
+for i in "${mac_array[@]}"
 do
-	file="Ar39_seed$i"
-	makeMacroFile $file $i & # Put a function in the background
-	makeMacroFile $file $i & # Put a function in the background
-
+   echo "Running: $prog $outputMacroDir$i"
+   $prog "$outputMacroDir""/""$i" &
 done
 
 ## Put all cust_func in the background and bash
