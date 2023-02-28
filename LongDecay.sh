0585f1076414de0e56fe912c106987f309dd4cf9
AustinMcDonald <austin.mcdonald96@mavs.uta.edu>
2023-02-28 12:41:55 -0600

    edit macro file to generate base of 10 sets

--- LongDecay.sh
+++ LongDecay.sh
@@ -30,56 +30,43 @@ function makeMacroFile {
 
 	# initialize run
 	echo "/run/initialize" >> $dest
-	echo "/random/setSeeds $2 707010" >> $dest
+	echo "/random/setSeeds 0 $2" >> $dest
 
 	# event ID offset
 	echo "/event/offset 0" >> $dest
 
 	# Supernova configs
-	echo "/Supernova/Event_Window 10 s" >> $dest
-	echo "/Supernova/Event_Cutoff 10 s" >> $dest
-
-  # single macro
-  # echo "/Supernova/N_Ar39_Decays 707000" >> $dest
-  # echo "/Supernova/N_Ar42_Decays 64" >> $dest
-  # echo "/Supernova/N_Bi214_Decays 7000" >> $dest
-  # echo "/Supernova/N_Co60_Decays 41" >> $dest
-  # echo "/Supernova/N_K40_Decays 12642" >> $dest
-  # echo "/Supernova/N_K42_Decays 64" >> $dest
-  # echo "/Supernova/N_Kr85_Decays 80500" >> $dest
-  # echo "/Supernova/N_Pb214_Decays 7000" >> $dest
-  # echo "/Supernova/N_Po210_Decays 5" >> $dest
-  # echo "/Supernova/N_Rn222_Decays 27740" >> $dest
-	# echo "/run/beamOn 1" >> $dest
+	echo "/Supernova/Event_Window 1000 s" >> $dest
+	echo "/Supernova/Event_Cutoff 1000 s" >> $dest
 
   # multiple macros
   echo "/Supernova/$3 1" >> $dest
 	echo "/run/beamOn $4" >> $dest
 }
 
-file="_Long_Decay"
-# single macro
-# makeMacroFile "Single"$file 2
-
 # multiple macros
-makeMacroFile "Ar39"$file 3 "N_Ar39_Decays" 707000
-makeMacroFile "Ar42"$file 3 "N_Ar42_Decays" 64
-makeMacroFile "Bi214"$file 3 "N_Bi214_Decays" 7000
-makeMacroFile "Co60"$file 3 "N_Co60_Decays" 41
-makeMacroFile "K40"$file 3 "N_K40_Decays" 12642
-makeMacroFile "K42"$file 3 "N_K42_Decays" 64
-makeMacroFile "Kr85"$file 3 "N_Kr85_Decays" 80500
-makeMacroFile "Pb214"$file 3 "N_Pb214_Decays" 7000
-makeMacroFile "Po210"$file 3 "N_Po210_Decays" 5
-makeMacroFile "Rn222"$file 3 "N_Rn222_Decays" 27740
-
-mac_array=( $( ls "$outputMacroDir""/" ) )
-## now loop through the above array
-for i in "${mac_array[@]}"
+for i in {1..10} 
 do
-   echo "Running: $prog $outputMacroDir$i"
-   $prog "$outputMacroDir""/""$i" &
+  echo "making macro.. $i"
+  makeMacroFile "Ar39"$file"_$i" "$i" "N_Ar39_Decays" 70700000
+  makeMacroFile "Ar42"$file"_$i" "$i" "N_Ar42_Decays" 6400
+  makeMacroFile "Bi214"$file"_$i" "$i" "N_Bi214_Decays" 700000
+  makeMacroFile "Co60"$file"_$i" "$i" "N_Co60_Decays" 4100
+  makeMacroFile "K40"$file"_$i" "$i" "N_K40_Decays" 1264200
+  makeMacroFile "K42"$file"_$i" "$i" "N_K42_Decays" 6400
+  makeMacroFile "Kr85"$file"_$i" "$i" "N_Kr85_Decays" 8050000
+  makeMacroFile "Pb214"$file"_$i" "$i" "N_Pb214_Decays" 700000
+  makeMacroFile "Po210"$file"_$i" "$i" "N_Po210_Decays" 500
+  makeMacroFile "Rn222"$file"_$i" "$i" "N_Rn222_Decays" 2774000
 done
+
+# mac_array=( $( ls "$outputMacroDir""/" ) )
+## now loop through the above array
+# for i in "${mac_array[@]}"
+# do
+#    echo "Running: $prog $outputMacroDir$i"
+#    $prog "$outputMacroDir""/""$i" &
+# done
 
 ## Put all cust_func in the background and bash
 ## would wait until those are completed
