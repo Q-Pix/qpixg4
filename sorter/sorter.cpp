c6f63103bd479a6de7cbb54514feca63f40746c1
AustinMcDonald <austin.mcdonald96@mavs.uta.edu>
2023-03-02 13:59:21 -0600

    fixed, working faster sort

--- sorter/sorter.cpp
+++ sorter/sorter.cpp
@@ -26,10 +26,11 @@ std::vector<size_t> sort_indexes(const std::vector<T> &v) {
 }
 int main(int argc, char** argv)
 {
-    if(argc != 3){
+    if(argc != 2){
         std::cout << "incorrect args";
         return -1;
     }
+    TTree::SetMaxTreeSize( 1000000000000LL ); // 1 TB
     TFile in_tf(argv[1], "UPDATE");
     TTree* in_tt = (TTree*)in_tf.Get("event_tree");
     int    m_hit_track_id_;
@@ -58,41 +59,72 @@ int main(int argc, char** argv)
     in_tt->SetBranchAddress("hit_length",         &m_hit_length_);
     in_tt->SetBranchAddress("hit_process_key",    &m_hit_process_key_);
     in_tt->SetBranchAddress("particle_id",        &m_particle_id);
+    in_tt->LoadBaskets(60000000000LL); // 60 GB
+    // in_tt->LoadBaskets(60e9);
 
     std::cout << "building entries..\n";
     std::vector<size_t> indices;
-    for(int i=0; i<in_tt->GetEntries(); ++i)
-        indices.push_back(m_hit_end_t_);
+    for(int i=0; i<in_tt->GetEntries(); ++i){
+        in_tt->GetEntry(i);
+        indices.push_back(m_hit_start_t_);
+    }
 
     std::cout << "sorting entries..\n";
     auto sorted = sort_indexes(indices);
+    std::cout << "sorted " << sorted.size() << " entries..\n";
 
     // TFile out_tf(argv[2], "RECREATE");
     TTree tt("sort_event_tree", "sort_event_tree");
-    tt.Branch("hit_track_id",       &m_hit_track_id_);
-    tt.Branch("hit_start_x",        &m_hit_start_x_);
-    tt.Branch("hit_start_y",        &m_hit_start_y_);
-    tt.Branch("hit_start_z",        &m_hit_start_z_);
-    tt.Branch("hit_start_t",        &m_hit_start_t_);
-    tt.Branch("hit_end_x",          &m_hit_end_x_);
-    tt.Branch("hit_end_y",          &m_hit_end_y_);
-    tt.Branch("hit_end_z",          &m_hit_end_z_);
-    tt.Branch("hit_end_t",          &m_hit_end_t_);
-    tt.Branch("hit_energy_deposit", &m_hit_energy_deposit_);
-    tt.Branch("hit_length",         &m_hit_length_);
-    tt.Branch("hit_process_key",    &m_hit_process_key_);
-    tt.Branch("particle_id",        &m_particle_id);
+    int    v_hit_track_id_;
+    double v_hit_start_x_;
+    double v_hit_start_y_;
+    double v_hit_start_z_;
+    double v_hit_start_t_;
+    double v_hit_end_x_;
+    double v_hit_end_y_;
+    double v_hit_end_z_;
+    double v_hit_end_t_;
+    double v_hit_length_;
+    double v_hit_energy_deposit_;
+    int v_hit_process_key_;
+    int v_particle_id;
+    tt.Branch("hit_track_id",       &v_hit_track_id_);
+    tt.Branch("hit_start_x",        &v_hit_start_x_);
+    tt.Branch("hit_start_y",        &v_hit_start_y_);
+    tt.Branch("hit_start_z",        &v_hit_start_z_);
+    tt.Branch("hit_start_t",        &v_hit_start_t_);
+    tt.Branch("hit_end_x",          &v_hit_end_x_);
+    tt.Branch("hit_end_y",          &v_hit_end_y_);
+    tt.Branch("hit_end_z",          &v_hit_end_z_);
+    tt.Branch("hit_end_t",          &v_hit_end_t_);
+    tt.Branch("hit_energy_deposit", &v_hit_energy_deposit_);
+    tt.Branch("hit_length",         &v_hit_length_);
+    tt.Branch("hit_process_key",    &v_hit_process_key_);
+    tt.Branch("particle_id",        &v_particle_id);
 
     int j = 0;
-    std::cout << "entries sorted.. building tree..\n";
+    std::cout << "Building tree..\n";
     for(auto i : sorted){
         in_tt->GetEntry(i);
+        v_hit_track_id_ = m_hit_track_id_;
+        v_hit_start_x_ = m_hit_start_x_;
+        v_hit_start_y_ = m_hit_start_y_;
+        v_hit_start_z_ = m_hit_start_z_;
+        v_hit_start_t_ = m_hit_start_t_;
+        v_hit_end_x_ = m_hit_end_x_;
+        v_hit_end_y_ = m_hit_end_y_;
+        v_hit_end_z_ = m_hit_end_z_;
+        v_hit_end_t_ = m_hit_end_t_;
+        v_hit_length_ = m_hit_length_;
+        v_hit_energy_deposit_ = m_hit_energy_deposit_;
+        v_hit_process_key_ = m_hit_process_key_;
+        v_particle_id = m_particle_id;
         tt.Fill();
         ++j;
-        if(j%100000==0) std::cout << "Filling Entry " << j << std::endl;
+        if(j%100000==0) std::cout << "Filling Entry " << j << ", from " << i << std::endl;
     }
     tt.Write();
-    // out_tf.Close();
+    // in_tf.Write();
 
     return 0;
 }
