78744e9e502ad6414827424765a72ce720b26c69
Guilherme Ruiz <57478888+guigoruiz1@users.noreply.github.com>
2023-11-29 17:35:31 +0000

    Update setup_cvmfs.sh

--- setup/setup_cvmfs.sh
+++ setup/setup_cvmfs.sh
@@ -6,13 +6,13 @@ setup git
 setup mrb
 setup cmake  v3_20_0
 setup boost  v1_75_0      -q e20:prof
-setup root   v6_22_08d    -q e20:p392:prof
+setup root   v6_26_06b    -q e20:p3913:prof
 setup marley v1_2_0e      -q e20:prof
-
-pushd ${MARLEY}/marley-`marley-config --version`/build
-source ${MARLEY}/marley-`marley-config --version`/setup_marley.sh
-popd
 
 pushd /cvmfs/geant4.cern.ch/geant4/10.7.p04a/x86_64-centos7-gcc9-optdeb-MT/share/Geant4-10.7.4/geant4make/
 source geant4make.sh
+popd
+
+pushd ${MARLEY}/marley-`marley-config --version`/build
+source ${MARLEY}/marley-`marley-config --version`/setup_marley.sh
 popd
